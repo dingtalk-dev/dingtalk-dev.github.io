1b47cddaac58795a18020f243f8d08719a957460
unixzii <unixzii@gmail.com>
2019-03-28 23:31:42 +0800

    Remove unused packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,6 @@ module.exports = {
         path: `${__dirname}/static`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
